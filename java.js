83e0486aa20efa10a10c37a6071f3beea4634559
Nkosinathi Prince Hiyashe <nphiyashe@gmail.com>
2024-03-07 20:54:26 +0000

    appedning time blocks to teh container

--- java.js
+++ java.js
@@ -3,11 +3,27 @@ $(document).ready(function() {
     $("#currentDay").text(dayjs().format('MMMM D, YYYY'));
 
   // Create timeblocks for standard business hours (9AM to 5PM)
-for (let hour = 9; hour <= 22; hour++) {
+for (let hour = 9; hour <= 17; hour++) {
     let row = $("<div>").addClass("row time-block");
     let timeCol = $("<div>").addClass("col-1 hour").text(dayjs().hour(hour).format('h A')).attr('data-hour', hour);
     let eventCol = $("<textarea>").addClass("col-10 description");
     let saveBtn = $("<button>").addClass("col-1 saveBtn").html('<i class="fas fa-save"></i>');
+
+    // Load events from local storage
+    eventCol.val(localStorage.getItem(hour));
+
+    // Color-code each timeblock based on past, present, and future
+    if (dayjs().hour() > hour) {
+        eventCol.addClass("past");
+    } else if (dayjs().hour() === hour) {
+        eventCol.addClass("present");
+    } else {
+        eventCol.addClass("future");
+    }
+
+    // Append timeblocks to the container
+    row.append(timeCol, eventCol, saveBtn);
+    $(".container").append(row);
 }
 
 })
